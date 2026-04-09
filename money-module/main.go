689cec01930519b84d9e5c6226b35258a4cea482
keyvi <z5482396@ad.unsw.edu.au>
2026-04-10 02:27:35 +1000

    removed unnecessary imports

--- money-module/main.go
+++ money-module/main.go
@@ -1,12 +1,10 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"os"
-	"google.golang.org/grpc"
+import ("fmt")
+
+/*	"google.golang.org/grpc"
 	. "github.com/tigerbeetle/tigerbeetle-go"
-)
+)*/
 
 func main() {
 	fmt.Println("Import ok!")
