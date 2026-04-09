571e97ad2827df9991b2f8f59601ecb48fa0fe1e
keyvi <z5482396@ad.unsw.edu.au>
2026-04-10 01:59:48 +1000

    moved money module off of bun and onto go

---
+++ money-module/go.mod
@@ -0,0 +1,5 @@
+module tbtest
+
+go 1.22.2
+
+require github.com/tigerbeetle/tigerbeetle-go v0.16.78
---
+++ money-module/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"google.golang.org/grpc"
+	. "github.com/tigerbeetle/tigerbeetle-go"
+)
+
+func main() {
+	fmt.Println("Import ok!")
+}
