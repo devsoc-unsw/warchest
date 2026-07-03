521ee860005b486913b26bd8a0acc8c9004c2a48
keyvi <z5482396@ad.unsw.edu.au>
2026-07-03 09:07:23 +0000

    Revert "added stuff to money module dir"
    This reverts commit d5201dfb8d61988b3c7d9c91cb375517c57928f3.

--- money-module/db/postgres/queries/transfers.sql
+++
@@ -1,9 +0,0 @@
--- name: CreateTransfer :exec
-INSERT INTO transfer (id, sender, recipient, description, transfer_type)
-VALUES (
-    $1, $2, $3, $4, $5
-);
-
--- name: ReadTransfer :one
-SELECT * FROM transfer
-WHERE id = $1;
