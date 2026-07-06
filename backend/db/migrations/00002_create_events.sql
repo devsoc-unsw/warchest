46160ac2ee859d03bb0b48ff96228caabcef0f47
Silvana Zhao <silvanazhao@Silvanas-MacBook-Pro.local>
2026-07-06 21:29:47 +1000

    create event domain

---
+++ backend/db/migrations/00002_create_events.sql
@@ -0,0 +1,19 @@
+-- +goose Up
+CREATE TYPE event_status AS ENUM ('DRAFT', 'IN_PROGRESS', 'CLOSED')
+
+CREATE TABLE events (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    event_name VARCHAR(255) NOT NULL,
+    event_time TIMESTAMPTZ,
+    budget NUMERIC(15, 2),
+    status event_status NOT NULL, 
+    location VARCHAR(255),
+    description VARCHAR(255),
+    society_id UUID NOT NULL,
+    created_by UUID NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
+
+-- +goose Down
+DROP TABLE events;
