f8563d6156a47f9f7b79caaa3e5c8d3d39a9e12b
keyvi <z5482396@ad.unsw.edu.au>
2026-07-27 18:57:31 +1000

    added init code, use client to talk to tb

--- money-module/main.go
+++ money-module/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	. "github.com/tigerbeetle/tigerbeetle-go"
+)
+
+func init_tb() {
+	tbAddress := os.Getenv("TB_ADDRESS")
+	if len(tbAddress) == 0 {
+		tbAddress = "3000"
+	}
+	client, err := NewClient(ToUint128(0), []string{tbAddress})
+	if err != nil {
+		log.Printf("Error creating client: %s", err)
+		return nil, err
+	}
+	return client, nil
+}
 
 func main() {
+	client, err := init_tb()
+	if err != nil {
+		log.Printf("Error creating client: %s", err)
+		return nil, err
+	}
+	defer client.Close()
 	fmt.Println("Hello, World!")
 }
